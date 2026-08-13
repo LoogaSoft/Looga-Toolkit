062a4bbd97fdadd0b2df8a95bb5b0e8d8f9f6160
Ben Holicek <bh4786@gmail.com>
2026-08-13 17:03:30 -0500

    Update MarkdownTextAssetEditor.cs

--- Editor/Tools/MarkdownTextAssetEditor.cs
+++ Editor/Tools/MarkdownTextAssetEditor.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
 using UnityEditor;
+using UnityEditor.AssetImporters;
 using UnityEngine;
 
 namespace LoogaSoft.Tools.Editor
